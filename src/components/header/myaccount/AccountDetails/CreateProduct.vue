4bdde23f7bf719713c633b8d97c7131fecbf06d5
Thomas <Thomas.azaria7@gmail.com>
2021-12-23 20:50:34 +1100

    account managment component update

--- src/components/header/myaccount/AccountDetails/CreateProduct.vue
+++ src/components/header/myaccount/AccountDetails/CreateProduct.vue
@@ -2,13 +2,18 @@
   <div class="createProductContainer">
     <!-- <h2>add a new product to store</h2> -->
 
-    <form @submit.prevent>
+    <form @submit.prevent="formData">
       <div class="formControl">
         <input placeholder="product name" type="text" v-model="name" />
         <input placeholder="short description" type="text" />
-        <textarea placeholder="describe your product" name id rows="10"></textarea>
+        <textarea
+          placeholder="describe your product"
+          name
+          id
+          rows="10"
+        ></textarea>
         <input type="number" min="0" value="0.00" />
-        <input type="file" />
+        <input type="file" multiple="multiple" @change="previewFiles" />
         <p>sort your product</p>
         <select v-model="item">
           <option value="Category" selected hidden>category</option>
@@ -29,10 +34,12 @@
         </select>
       </div>
 
-      <button @click="formData">submit</button>
+      <button type="submit">submit</button>
     </form>
 
-    <div class="preview"></div>
+    <div class="preview">
+      <img src="319022_2387118763915_6102261_n.jpg" alt="" />
+    </div>
   </div>
 </template>
 <script>
@@ -40,20 +47,26 @@ export default {
   data() {
     return {
       item: "",
-      name: ""
+      name: "",
+      fileData: {},
     };
   },
   computed: {
     myformData() {
       return console.log(this.item);
-    }
+    },
   },
   methods: {
     formData() {
       console.log(this.item);
       console.log(this.name);
-    }
-  }
+      console.log(this.fileData);
+    },
+    previewFiles(event) {
+      this.fileData = event.target.files[0];
+      console.log(event.target.files[0]);
+    },
+  },
 };
 </script>
 
--- src/components/header/myaccount/AccountManagement.vue
+++ src/components/header/myaccount/AccountManagement.vue
@@ -8,78 +8,89 @@
         <h2 v-if="authState">store name</h2>
         <h4>
           <span v-if="authState">
-            Welcome: {{authState.displayName}}
-            <i @click="logout" class="fas fa-sign-out-alt"></i>
+            Welcome: {{ authState.displayName }}
+            <i style="" @click="logout" class="fas fa-sign-out-alt">logOut</i>
           </span>
-          <span style="padding:20px" v-else>Please login or register</span>
+          <span style="padding: 20px" v-else>Please login or register</span>
         </h4>
       </div>
     </div>
 
     <div v-if="authState" class="accountContainer__nav">
       <ul>
-        <li>
-          <router-link @click="showCategory('account')" to="/my-account/authentication">account</router-link>
+        <li class="navSelector">
+          <a @click="showCategory('account')" to="/my-account/authentication"
+            >account</a
+          >
+          <div v-if="subCategoryActive === 'account'" class="subCategory">
+            <router-link style="color: black" to="/my-account/authentication"
+              >Login / Register</router-link
+            >
+            <router-link style="color: black" to="/my-account/profile"
+              >My Profile</router-link
+            >
+            <!-- <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link> -->
+          </div>
         </li>
-        <li>
-          <router-link @click="showCategory('plan')" to="/my-account/subscription">subscription</router-link>
-        </li>
-        <li>
-          <router-link @click="showCategory('bank')" to="/my-account/Statemnt">Bank StatMent</router-link>
-        </li>
-
         <li>
           <router-link
-            @click="showCategory('stock')"
-            to="/my-account/manage_products"
-          >Stock management</router-link>
+            @click="showCategory('plan')"
+            to="/my-account/subscription"
+            >subscription</router-link
+          >
+        </li>
+        <li class="navSelector">
+          <a @click="showCategory('bank')" to="/my-account/Statemnt"
+            >Bank StatMent</a
+          >
+          <div v-if="subCategoryActive === 'bank'" class="subCategory">
+            <!-- <router-link style="color: black" to="/my-account/profile"
+              >CreateProfile</router-link
+            >
+            <router-link style="color: black" to="/my-account/profile"
+              >CreateProfile</router-link
+            >
+            <router-link style="color: black" to="/my-account/profile"
+              >CreateProfile</router-link
+            > -->
+          </div>
+        </li>
+
+        <li class="navSelector">
+          <a @click="showCategory('stock')" to="/my-account/manage_products"
+            >Stock management</a
+          >
+          <div v-if="subCategoryActive === 'stock'" class="subCategory">
+            <router-link style="color: black" to="/my-account/create_product"
+              >add product</router-link
+            >
+            <router-link style="color: black" to="/my-account/manage_products"
+              >edit my stock</router-link
+            >
+            <!-- <router-link style="color: black" to="/my-account/profile"></router-link> -->
+          </div>
+        </li>
+
+        <li class="navSelector">
+          <a @click="showCategory('buy_history')">Purchase history</a>
+          <div v-if="subCategoryActive === 'buy_history'" class="subCategory">
+            <router-link style="color: black" to="/my-account/BuyerRecipt"
+              >Buyer Recipt</router-link
+            >
+            <router-link style="color: black" to="/my-account/SellerRecipt"
+              >Seller Recipts</router-link
+            >
+
+            <!--  <router-link style="color:black" to="/my-account/profile">CreateProfile</router-link> -->
+          </div>
         </li>
 
         <li>
-          <router-link
-            @click="showCategory('buy_history')"
-            to="/my-account/BuyerRecipt"
-          >Purchase history</router-link>
-        </li>
-
-        <li>
-          <router-link @click="showCategory('policy')" to="/my-account/policy">policy & privacy</router-link>
+          <router-link @click="showCategory('policy')" to="/my-account/policy"
+            >policy & privacy</router-link
+          >
         </li>
       </ul>
-
-      <div class="dropDown">
-        <div v-if="subCategoryActive === 'account'" class="subCategory">
-          <router-link style="color: black" to="/my-account/authentication">Login / Register</router-link>
-          <router-link style="color: black" to="/my-account/profile">My Profile</router-link>
-          <!-- <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link> -->
-        </div>
-        <div v-if="subCategoryActive === 'bank'" class="subCategory">
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-        </div>
-
-        <div v-if="subCategoryActive === 'buy_history'" class="subCategory">
-          <router-link style="color: black" to="/my-account/BuyerRecipt">BuyerRecipt</router-link>
-          <router-link style="color: black" to="/my-account/SellerRecipt">SellerRecipts</router-link>
-          <!--  <router-link style="color:black" to="/my-account/profile">CreateProfile</router-link> -->
-        </div>
-        <div v-if="subCategoryActive === 'stock'" class="subCategory">
-          <router-link style="color: black" to="/my-account/create_product">add product</router-link>
-          <router-link style="color: black" to="/my-account/manage_products">edit my stock</router-link>
-          <!-- <router-link style="color: black" to="/my-account/profile"></router-link> -->
-        </div>
-        <!-- <div v-if="subCategoryActive === 'plan'" class="subCategory">
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-        </div>-->
-        <!-- <div v-if="subCategoryActive === 'policy'" class="subCategory">
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-          <router-link style="color: black" to="/my-account/profile">CreateProfile</router-link>
-        </div>-->
-      </div>
     </div>
     <div class="accountContainer__view">
       <router-view></router-view>
@@ -95,7 +106,7 @@ export default {
   },
   data() {
     return {
-      subCategoryActive: ""
+      subCategoryActive: "",
     };
   },
   mounted() {
@@ -109,7 +120,7 @@ export default {
       const auth = this.$store.getters["UserState/getAuthState"];
       console.log(auth);
       return auth;
-    }
+    },
   },
   methods: {
     logout() {
@@ -120,15 +131,15 @@ export default {
     },
     getProductList() {
       return fetch("http://localhost:3000/my-server/product", {
-        method: "GET"
+        method: "GET",
       })
-        .then(function(res) {
+        .then(function (res) {
           //   console.log(data);
           console.log(res);
 
           return res.json();
         })
-        .then(function(data) {
+        .then(function (data) {
           console.log(data);
           return data.id;
         });
@@ -148,21 +159,21 @@ export default {
       return fetch("http://localhost:3000/my-server/pay-clients", {
         method: "POST",
         headers: {
-          "content-type": "application/json"
-        }
+          "content-type": "application/json",
+        },
       })
-        .then(function(res) {
+        .then(function (res) {
           //   console.log(data);
           console.log(res);
 
           return res.json();
         })
-        .then(function(data) {
+        .then(function (data) {
           console.log(data);
           return data.id;
         });
-    }
-  }
+    },
+  },
 };
 </script>
 <style lang="scss">
@@ -200,15 +211,25 @@ export default {
       padding: 20px 50px;
       border: solid 2px rgba(0, 0, 0, 0.137);
       border-left: 0;
+      width: 20%;
       // border-radius: 0 5% 5% 0;
       background-color: #ffffffef;
+
+      i {
+        position: absolute;
+        top: 30%;
+        right: 30px;
+        font-size: 1.5vw;
+      }
       h2 {
-        margin: 2px;
+        margin: 0;
         text-transform: uppercase;
         // text-align: right;
+        font-size: 1.5vw;
       }
       h4 {
         margin: 0;
+        font-size: 1vw;
       }
     }
   }
@@ -225,33 +246,27 @@ export default {
   }
 
   &__nav {
-    padding: 0 10%;
-    background-color: rgb(255, 255, 255);
-    // background-image: url("https://images.pexels.com/photos/10482178/pexels-photo-10482178.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
-    position: relative;
+    padding: 50px 10%;
 
-    background-size: cover;
-    background-position-y: 300px;
-    display: flex;
-    align-items: center;
-    .dropDown {
-      display: flex;
-      height: 120px;
+    .navSelector {
+      a {
+        cursor: pointer;
+      }
       .subCategory {
-        // background-image: url("https://images.pexels.com/photos/3208282/pexels-photo-3208282.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940");
-        // height: 100px;
-        // clip-path: polygon(0 0, 100% 0%, 100% 100%, 0 50%);
-        // padding: 10px;
-        box-sizing: border-box;
-        text-align: right;
-        flex: 1 0 25%;
+        position: absolute;
         display: flex;
         flex-direction: column;
+        background-color: rgb(253, 221, 221);
+        width: 100%;
+        top: 100%;
+
         a {
           font-size: 15px;
-          // background-color: rgba(161, 161, 161, 0.226);
           text-decoration: none;
           padding: 4px 10px;
+          &:hover {
+            background-color: rgba(240, 230, 140, 0.267);
+          }
           // margin: 5px;
         }
       }
@@ -269,6 +284,7 @@ export default {
       // padding: 10px;
 
       li {
+        position: relative;
         text-align: center;
         padding: 10px 0;
         margin: 5px 0;
